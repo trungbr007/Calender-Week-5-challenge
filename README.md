2ad36cf28fa81994f44f517707f3a53f84d8520c
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-03-27 00:33:14 -0400

    get Item from local storage

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work Day Scheduler Starter Code
+
+
+
+
+
+
+
+
+* The URL of the deployed application: https://trungbr007.github.io/Calender-Week-5-challenge/
+* Github Link: https://github.com/trungbr007/Calender-Week-5-challenge
+* <i= src="./Screen Shot 2022-03-27 at 12.03.23 AM.png">
--- script.js
+++ script.js
@@ -42,7 +42,15 @@ $(document).ready(function() {
 
 });
   
-$('.col-8').textContent=localStorage.getItem(text);
+$('#9').val(localStorage.getItem('9'));
+$('#10').val(localStorage.getItem('10'));
+$('#11').val(localStorage.getItem('11'));
+$('#12').val(localStorage.getItem('12'));
+$('#13').val(localStorage.getItem('13'));
+$('#14').val(localStorage.getItem('14'));
+$('#15').val(localStorage.getItem('15'));
+$('#16').val(localStorage.getItem('16'));
+$('#17').val(localStorage.getItem('17'));
 
 
     
